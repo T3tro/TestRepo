96c1027bad58944a24ec37ef887f57035ac0801a
Shenol Osman <7887878+x3nol@users.noreply.github.com>
2019-03-17 08:47:02 +0100

    Windows Forms ile galeri uygulaması denemesi

---
+++ GaleriUygulamasi/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace GaleriUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuDosya = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuAc = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuKaydet = new System.Windows.Forms.ToolStripMenuItem();
+            this.flpResimPaneli = new System.Windows.Forms.FlowLayoutPanel();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuEkle = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuCikis = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuDosya});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuDosya
+            // 
+            this.mnuDosya.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuAc,
+            this.mnuKaydet,
+            this.toolStripSeparator1,
+            this.mnuEkle,
+            this.toolStripSeparator2,
+            this.mnuCikis});
+            this.mnuDosya.Name = "mnuDosya";
+            this.mnuDosya.Size = new System.Drawing.Size(62, 24);
+            this.mnuDosya.Text = "Dosya";
+            // 
+            // mnuAc
+            // 
+            this.mnuAc.Name = "mnuAc";
+            this.mnuAc.Size = new System.Drawing.Size(216, 26);
+            this.mnuAc.Text = "Aç";
+            this.mnuAc.Click += new System.EventHandler(this.mnuAc_Click);
+            // 
+            // mnuKaydet
+            // 
+            this.mnuKaydet.Name = "mnuKaydet";
+            this.mnuKaydet.Size = new System.Drawing.Size(216, 26);
+            this.mnuKaydet.Text = "Kaydet";
+            this.mnuKaydet.Click += new System.EventHandler(this.mnuKaydet_Click);
+            // 
+            // flpResimPaneli
+            // 
+            this.flpResimPaneli.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flpResimPaneli.Location = new System.Drawing.Point(0, 28);
+            this.flpResimPaneli.Name = "flpResimPaneli";
+            this.flpResimPaneli.Size = new System.Drawing.Size(800, 422);
+            this.flpResimPaneli.TabIndex = 1;
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(213, 6);
+            // 
+            // mnuEkle
+            // 
+            this.mnuEkle.Name = "mnuEkle";
+            this.mnuEkle.Size = new System.Drawing.Size(216, 26);
+            this.mnuEkle.Text = "Ekle";
+            this.mnuEkle.Click += new System.EventHandler(this.mnuEkle_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(213, 6);
+            // 
+            // mnuCikis
+            // 
+            this.mnuCikis.Name = "mnuCikis";
+            this.mnuCikis.Size = new System.Drawing.Size(216, 26);
+            this.mnuCikis.Text = "Çıkış";
+            this.mnuCikis.Click += new System.EventHandler(this.mnuCikis_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flpResimPaneli);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Winforms Galeri Uygulaması";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuDosya;
+        private System.Windows.Forms.ToolStripMenuItem mnuAc;
+        private System.Windows.Forms.ToolStripMenuItem mnuKaydet;
+        private System.Windows.Forms.FlowLayoutPanel flpResimPaneli;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem mnuEkle;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem mnuCikis;
+    }
+}
+
---
+++ GaleriUygulamasi/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GaleriUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mnuAc_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Galeri Aç";
+            ofd.Filter = "Galeri Dosyalari(*.xenol)|*.xenol";
+            ofd.FilterIndex = 1;
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string[] dosyalar = System.IO.File.ReadAllLines(ofd.FileName);
+                flpResimPaneli.Controls.Clear();
+                foreach (string item in dosyalar)
+                {
+                    PicturesBoxEkle(item);
+
+                }
+            }
+
+        }  
+             
+        private void mnuKaydet_Click(object sender, EventArgs e)
+        {
+            List<string> kaydedilecekDosyaKonumlari = new List<string>();
+
+            foreach (Panel item in flpResimPaneli.Controls)
+            {
+                PictureBox pbox = (PictureBox)item.Controls[0];
+                kaydedilecekDosyaKonumlari.Add(pbox.ImageLocation);
+
+            }
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "Galeri Kaydet";
+            sfd.Filter = "Galeri Dosyalari(*.xenol)|*.xenol";
+            sfd.FilterIndex = 1;
+            sfd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            if (sfd.ShowDialog()==DialogResult.OK)
+            {
+                System.IO.File.WriteAllLines(sfd.FileName, kaydedilecekDosyaKonumlari);
+                MessageBox.Show("Galeri Kaydedilmiştir.");
+            }
+        }       
+
+        private void mnuEkle_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "JPEG Resimler(*.jpg;*.jpeg)|*.jpg;*.jpeg|PNG Resimler(*.png)|*.png|Tüm Dosyalar(*.jpg;*.jpeg;*.png;*.bmp)|*.jpg;*.jpeg;*.png;*.bmp";
+            ofd.FilterIndex = 3;
+            ofd.Title = "Resimleri Seç";
+            ofd.Multiselect = true;
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string[] dosyaKonumlari = ofd.FileNames;
+                foreach (string item in dosyaKonumlari)
+                {
+                    PicturesBoxEkle(item);
+                }
+            }
+
+        }
+        private void mnuCikis_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void PicturesBoxEkle(string dosyaKonumu)
+        {
+            Panel pnl = new Panel();
+            pnl.BorderStyle = BorderStyle.FixedSingle;
+            pnl.Width = 75;
+            pnl.Height = 75;
+            PictureBox pb = new PictureBox();
+            pb.SizeMode = PictureBoxSizeMode.Zoom;
+            pb.ImageLocation = dosyaKonumu;
+            pb.Dock = DockStyle.Fill;
+
+            pb.Click += Pb_Click;
+
+            pnl.Controls.Add(pb);
+            flpResimPaneli.Controls.Add(pnl);
+        }
+        private void Pb_Click(object sender, EventArgs e)
+        {
+            PictureBox pbox = (PictureBox)sender;
+
+            Form frm = new Form();
+            frm.Text = "Resim Önizleme";
+            frm.Width = 300;
+            frm.Height = 300;
+
+            PictureBox pbnew = new PictureBox();
+            pbnew.ImageLocation = pbox.ImageLocation;
+            pbnew.SizeMode = pbox.SizeMode;
+            pbnew.Dock = DockStyle.Fill;
+            frm.Controls.Add(pbnew);
+            frm.Show();
+        }
+    }
+}
